565a8225d15c03dab6c75f2c7cce2a0b931323e7
unknown <Uporabnik@UC102-10.adcenter.scptuj.si>
2024-10-15 10:26:05 +0200

    spletna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeBoxComponent } from "./welcome-box/welcome-box.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NavbarButtonsComponent } from "./navbar-buttons/navbar-buttons.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, WelcomeBoxComponent, FooterComponent, NavbarButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/welcome-box/welcome-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-box.component.html',
+  styleUrl: './welcome-box.component.scss'
+})
+export class WelcomeBoxComponent {
+
+}
